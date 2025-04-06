dc87d47a3775372895e4f4fd17dae6f48cd906d3
vandanafuloria <vandanafuloria02@gmail.com>
2025-04-06 23:12:58 +0530

    inverted right and left tringle added

--- patterns-printing/index.js
+++ patterns-printing/index.js
@@ -9,4 +9,34 @@ function printLeftTriangle(n) {
   }
 }
 
-printLeftTriangle(20);
+printLeftTriangle(10);
+
+function printRightTriangle(n) {
+  let space = ""; // intial spaces
+  let star = "";
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      space += " ";
+    }
+    for (let k = 0; k <= i; k++) {
+      star += "*";
+    }
+    console.log(space + star); // both merges in the same line ,
+    space = ""; // empty both the value to fill with new value again
+    star = "";
+  }
+}
+printRightTriangle(10);
+
+function invertedLeftTringle(n) {
+  let star = "";
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n - i; j++) {
+      star += "*";
+    }
+    console.log(star);
+    star = "";
+  }
+}
+
+invertedLeftTringle(10);
