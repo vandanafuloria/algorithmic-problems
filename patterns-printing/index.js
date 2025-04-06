0a79c579af6ac2b7c2d4beb7420c7ab97d29f2d8
vandanafuloria <vandanafuloria02@gmail.com>
2025-04-06 23:34:04 +0530

    paint triangle function added

--- patterns-printing/index.js
+++ patterns-printing/index.js
@@ -10,7 +10,7 @@ function printLeftTriangle(n) {
 }
 
 printLeftTriangle(10);
-
+//******************************************************************* */
 function printRightTriangle(n) {
   let space = ""; // intial spaces
   let star = "";
@@ -27,7 +27,7 @@ function printRightTriangle(n) {
   }
 }
 printRightTriangle(10);
-
+//**************************************************** */
 function invertedLeftTringle(n) {
   let star = "";
   for (let i = 0; i < n; i++) {
@@ -40,3 +40,41 @@ function invertedLeftTringle(n) {
 }
 
 invertedLeftTringle(10);
+
+//************************************************* */
+
+function invertedRightTriangle(n) {
+  let star = "";
+  let space = "";
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < i; j++) {
+      space += " ";
+    }
+    for (let k = 0; k < n - i; k++) {
+      star += "*";
+    }
+    console.log(space + star);
+    space = "";
+    star = "";
+  }
+}
+
+invertedRightTriangle(10);
+//****************************************************************** */
+function printTringle(n) {
+  let space = "";
+  let star = "";
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n - 1 - i; j++) {
+      space += " ";
+    }
+    for (let k = 0; k <= i * 2 + 1; k++) {
+      star += "*";
+    }
+    console.log(space + star);
+    space = "";
+    star = "";
+  }
+}
+printTringle(5);
